1180e511275205e1f44f4acdf03ea9af16b32ad5
Matheus Henrique <matheushenriquedasilvaa.2021@gmail.com>
2021-12-27 16:51:02 -0300

    delete, update, create and view

--- tasks/urls.py
+++ tasks/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.taskList, name='task-list'),
-    path('task/<int:id>', views.taskView, name="task-view")
+    path('task/<int:id>', views.taskView, name="task-view"),
+    path('newtask/', views.newTask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
 ]
